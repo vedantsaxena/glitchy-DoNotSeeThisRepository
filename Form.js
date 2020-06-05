0bab7730d2f56bf038d8e265afe13fbb735d38a9
vedantsaxena <64244974+vedantsaxena@users.noreply.github.com>
2020-06-05 10:04:18 +0530

    Add files via upload

---
+++ Form.js
@@ -0,0 +1,35 @@
+class Form {
+
+  constructor() {
+    this.input = createInput("Name");
+    this.button = createButton('Play');
+    this.greeting = createElement('h2');
+  }
+  hide(){
+    this.greeting.hide();
+    this.button.hide();
+    this.input.hide();
+  }
+
+  display(){
+    var title = createElement('h2')
+    title.html("Car Racing Game");
+    title.position(windowWidth/2-50, 0);
+
+    this.input.position(windowWidth/2-50, windowHeight/2-30);
+    this.button.position(windowWidth/2 , windowHeight/2+20);
+
+    this.button.mousePressed(()=>{
+      this.input.hide();
+      this.button.hide();
+      player.name = this.input.value();
+      playerCount+=1;
+      player.index = playerCount;
+      player.update();
+      player.updateCount(playerCount);
+      this.greeting.html("Hello " + player.name)
+      this.greeting.position(windowWidth/2-50,windowHeight/4);
+    });
+
+  }
+}
